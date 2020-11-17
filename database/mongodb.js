bd7365163074325af7e9c317ba7e88599e3c9a06
Marcos Vinicius <marcosvinicius.udia1256@gmail.com>
2020-11-17 20:12:11 -0300

    server mongo

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 //host:porta:banco 
-const connectionURL = 'mongodb://172.17.0.2:27017/unateste'
+// const connectionURL = 'mongodb://172.17.0.2:27017/unateste'
+const connectionURL = 'mongodb://localhost:27017/UNA'
 // 'mongodb://localhost:27017/UNA'
 
 mongoose.connect(connectionURL, {
