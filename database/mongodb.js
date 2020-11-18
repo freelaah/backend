027a494e0acff62c347167153311d843bde10fe2
Moises Guilherme <moisescomp@gmail.com>
2020-11-17 22:14:02 -0300

    connecao no monbodb atlas

--- database/mongodb.js
+++ database/mongodb.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 //host:porta:banco 
 // const connectionURL = 'mongodb://172.17.0.2:27017/unateste'
-const connectionURL = 'mongodb://localhost:27017/UNA'
+//const connectionURL = 'mongodb://localhost:27017/UNA'
+const connectionURL = "mongodb+srv://moises:teste@cluster0.wrxxf.mongodb.net/<dbname>?retryWrites=true&w=majority";
 // 'mongodb://localhost:27017/UNA'
+
+
 
 mongoose.connect(connectionURL, {
     useNewUrlParser: true,
